df5d6379f6b50180095dfec5509bdad9911864d5
Victor Feyens <vfe@odoo.com>
2021-01-06 15:54:09 +0100

    js cleanup

--- extensions/odoo_ext/static/js/page_toc.js
+++ extensions/odoo_ext/static/js/page_toc.js
@@ -2,8 +2,6 @@
 
     document.addEventListener('DOMContentLoaded', function () {
 
-        // TODO ANV/VFE move in separate file
-
         // Hide the local table of contents if empty
         // there is always one element (hidden by css) which is the title of the page
         // + the sections defined on the page
