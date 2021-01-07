6b7614aed21280492f954aefadb86a6f4459469a
Antoine Vandevenne (anv) <anv@odoo.com>
2021-01-07 11:50:33 +0100

    flag first heading ref in local toc

--- extensions/odoo_ext/static/js/page_toc.js
+++ extensions/odoo_ext/static/js/page_toc.js
@@ -1,21 +1,38 @@
 (function ($) {
 
-    document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', () => {
+        const pageTOC = document.getElementById('o_page_toc'); // The <aside> elem of the local toc
+        if (pageTOC) { // The local toctree is not included for toctree pages (see layout.html)
+            const headingRefs = pageTOC.querySelectorAll('a'); // The references to all headings
 
-        // Hide the local table of contents if empty
-        // there is always one element (hidden by css) which is the title of the page
-        // + the sections defined on the page
-        const pageMenu = document.getElementById('o_on_page'); // aside
-        if (pageMenu) {
-            // on toctree pages, the sidebar isn't shown (on jinja level)
-            const menuItems = pageMenu.querySelectorAll('li');
+            // Always hide the TOC entry targeting the title (<h1> heading)
+            _flagFirstHeadingRef(headingRefs);
 
-            if (menuItems.length <= 2) {
-                // change to 1 if we want to show tocs with only one section.
-                pageMenu.style.visibility = 'hidden';
+            // If the page TOC has less than 2 headings, in addition to the title, hide it entirely
+            if (headingRefs.length <= 2) {
+                _hidePageTOC(pageTOC);
             }
-
         }
-
     });
+
+    /**
+     * Add the class `o_page_toc_title` on the first heading reference.
+     *
+     * @param {NodeList} headingRefs - The references to the headings of the page
+     * @return {undefined}
+     */
+    const _flagFirstHeadingRef = (headingRefs) => {
+        headingRefs[0].classList.add('o_page_toc_title');
+    };
+
+    /**
+     * Entirely hide the local tree of contents.
+     *
+     * @param {object} pageTOC - The tree of contents of the page
+     * @return {undefined}
+     */
+    const _hidePageTOC = (pageTOC) => {
+        pageTOC.style.visibility = 'hidden';
+    }
+
 })();
